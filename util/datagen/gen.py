4260d163a050bb76ab2af6ad63483edd949729d0
JinKeonsu <keonsu.jin@gmail.com>
2017-07-18 12:44:53 +0900

    목적지 패턴 추가

--- util/datagen/gen.py
+++ util/datagen/gen.py
@@ -6,6 +6,14 @@ file = open("requests.txt", "w")
 
 for station in stations:
     for command in cmdPatterns:
+        strRequest = station + '행 열차 ' + command
+        file.write(strRequest+'\n')
         strRequest = station + '가는 열차 ' + command
         file.write(strRequest+'\n')
+        strRequest = station + '행 기차 ' + command
+        file.write(strRequest+'\n')
+        strRequest = station + '가는 기차 ' + command
+        file.write(strRequest+'\n')
     print(station)
+
+file.close()
